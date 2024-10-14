53652f796c3d8641a79fc65a67f456fee7cb62b6
Young <772478760@qq.com>
2024-10-14 14:02:59 +0800

    任意公差

--- sum.py
+++ sum.py
@@ -11,6 +11,7 @@ def compute_coefficients(begin, step, power):
     for i in range(0, power + 1):
         cur_x += step
         points.append((Fraction(cur_x), Fraction(cur_x ** power)))
+        # 可以增量计算a和b，时间复杂度可以优化到O(k), k是幂指数
         a_b = get_linear_regression_params(points)
         a.append((cur_x, a_b[0]))
         b.append((cur_x, a_b[1]))
@@ -18,7 +19,9 @@ def compute_coefficients(begin, step, power):
     b_coeffs = lagrange_interpolation_coefficients(b[:-1])
     a_coeffs = lagrange_interpolation_coefficients(a)
 
+    # 等差数列求和的常数项
     sn_const_term = Fraction(begin, 2) - Fraction(begin ** 2, 2 * step)
+    # 等差数列求和的常数项
     term_count_const_term = Fraction(1) - Fraction(begin, step)
 
     result_coeffs = [Fraction(0) for _ in range(power + 2)]
@@ -34,7 +37,8 @@ def compute_coefficients(begin, step, power):
     return result_coeffs
 
 
-# 手写的拉格朗日插值法，返回多项式的系数
+# 拉格朗日插值法，返回多项式的系数
+# 拉格朗日插值法时间复杂度O(k^2)，使用FFT优化后的时间复杂度可以降低到O(klogk)，k是幂指数
 def lagrange_interpolation_coefficients(points):
     x_values = [i[0] for i in points]
     y_values = [i[1] for i in points]
@@ -75,10 +79,20 @@ def get_linear_regression_params(points: [tuple]) -> tuple:
 
 def check(begin, step, power, coeffs):
     n = 50000
+    if step < 0:
+        n = -n
     sum = Fraction(0)
-    for i in range(begin, n + 1, step):
-        n = i
+    i = begin
+    while True:
+        if step < 0:
+            if i < n:
+                break
+        else:
+            if i > n:
+                break
         sum += i ** power
+        i += step
+    n = i - step
 
     tmp = Fraction(0)
     p = Fraction(1)
@@ -87,14 +101,17 @@ def check(begin, step, power, coeffs):
         p *= n
     if sum != tmp:
         raise Exception(f"illegal, begin:{begin}, step:{step}, power:{power}, n:{n}, expected:{sum}, real:{tmp}")
-    print(f'check ok, n = {n}')
+    print(f'check ok, n = {float(n)}')
 
 
 def main():
-    step = 1
-    begin = 1
+    # 底数的公差，可以是任意实数
+    step = Fraction.from_float(-0.6)
+    # 最小底数，可以是任意实数
+    begin = Fraction.from_float(-12.34)
+    # 幂指数，可以是任意正整数
     for power in range(1, 300):
-        print(f"\nbegin:{begin}, step:{step}, power:{power}")
+        print(f"\nbegin:{float(begin)}, step:{float(step)}, power:{power}")
         result_coeffs = compute_coefficients(begin, step, power)
         for i, coef in enumerate(reversed(result_coeffs)):
             # print(f"{len(result_coeffs) - i - 1}: {coef}   ", end='')
